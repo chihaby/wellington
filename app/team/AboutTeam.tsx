97b3a2291287046ed750d8818dbf0b0ff211c5aa
Radouane Chihaby <chihaby@gmail.com>
2026-01-16 16:15:57 -0800

    updates changes from client

--- app/team/AboutTeam.tsx
+++ app/team/AboutTeam.tsx
@@ -20,29 +20,64 @@ export const metadata: Metadata = {
 
 const teamMembers = [
   {
-    name: "Alex Johnson",
-    role: "Frontend Developer",
+    name: "Ricardo da Silva",
+    role: "Wellington Property Company",
     about:
-      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+      "With over fifteen years of experience providing commercial brokerage {'&'}amp; consulting services, Mr.Ricardo da Silva has successfully represented numerous user, investor and developer clients in a variety of commercial property purchase, sale and lease transactions. Mr. da Silva is a graduate of Boston College.",
     image: male_icon,
   },
   {
-    name: "Maria Lopez",
-    role: "UX Designer",
+    name: "Tad Lacey",
+    role: "CMA Asset Managers, Inc.r",
     about:
       "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
     image: female_icon,
   },
   {
-    name: "James Smith",
-    role: "Backend Engineer",
+    name: "Chris Gray",
+    role: "Senior Asset Manager",
     about:
       "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
     image: male_icon,
   },
   {
-    name: "Sarah Kim",
-    role: "Product Manager",
+    name: "Christine Green",
+    role: "CMA Asset Managers, Inc.",
+    about:
+      "Boss Ninja Lady A rampant generalist who oversees all aspects of the business. Always looking for opportunities for optimization and to complete her lifetime certification in Kaizen. Proactive manager who resists a reactive environment. Always wishing her team gets to do exactly what they want, while fulfilling the needs of the company. A double Major BA from SFSU, apparently Christine still speaks some Spanish and French. Christine has lived the majority of her life in the Bay Area. Along with her resistant family, she is a budding naturalist with a distinct caterpillar problem. Desperately seeking new canine companion to complete her family. Will settle for bird watching on weekends.",
+    image: female_icon,
+  },
+  {
+    name: "Michelle Hardy",
+    role: "CMA Asset Managers, Inc. Senior Administrator",
+    about:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+    image: female_icon,
+  },
+  {
+    name: "Raynald Jean-Gilles",
+    role: "CMA Asset Managers, Inc. Assistant Asset Manager",
+    about:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+    image: male_icon,
+  },
+  {
+    name: "D. Holt Lacey",
+    role: "CMA Asset Managers, Inc. Facilities and Asset Manager",
+    about:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+    image: male_icon,
+  },
+  {
+    name: "Aja Latham",
+    role: "CMA Asset Managers, Inc. Office Administrator",
+    about:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+    image: female_icon,
+  },
+  {
+    name: "Kelly Tran",
+    role: "Wellington Property Company Senior Operation Manager",
     about:
       "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
     image: female_icon,
@@ -58,16 +93,31 @@ export default function Team() {
           <h1>Meet the Team</h1>
           <p>The people who make everything possible</p>
         </header>
-        <div >
+        <div className={styles.image_wrapper}>
           <Image
-          className={styles.image_wrapper}
             src={partners}
             alt="Company logo"
-            width={300}
-            height={120}
+            width={1200}
             style={{ maxWidth: "100%", height: "auto" }}
           />
         </div>
+        <div>
+          <p>
+            <em>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book. It has survived not only five
+              centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s
+              with the release of Letraset sheets containing Lorem Ipsum
+              passages, and more recently with desktop publishing software like
+              Aldus PageMaker including versions of Lorem Ipsum
+            </em>
+          </p>
+        </div>
+        <br />
+        <h2>Team Members</h2>
         <section className={styles.team_grid}>
           {teamMembers.map((member, index) => (
             <div key={index} className={styles.team_card}>
